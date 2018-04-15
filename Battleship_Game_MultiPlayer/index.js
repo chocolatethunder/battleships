4e316f3c5a2166d0cff254a85cdd1db4eb6e6706
WilliamLingZ <WilliamLingZ@gmail.com>
2018-04-14 23:58:48 -0600

    battleships game is done, just need to implement chat probably and like fix up the css so its not so ugly and toss in mikes materials

--- Battleship_Game_MultiPlayer/index.js
+++ Battleship_Game_MultiPlayer/index.js
@@ -6,8 +6,36 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+max_connections = 2;
+current_connections = 0;
+
 io.on('connection', function(socket){
-  console.log('a user connected');
+	console.log('a user connected');
+	//if a connecting user would increase the number of users we have to more than 2, disconnect them.
+	if(current_connections >= max_connections) {
+		socket.disconnect();
+		console.log('too many users, disconnecting user');
+	} else {
+		//increment and decrement user count when they join or leave
+		current_connections++;
+		socket.on('disconnect', function() {
+			current_connections--;
+		});
+	}
+	
+	socket.on('transferTorpedo', function(eid){
+		socket.broadcast.emit('receiveTorpedo', eid);
+	});
+	
+	socket.on('transferBoard', function(playerBoard) {
+		var newBoard = playerBoard;
+		socket.broadcast.emit('receiveBoard', newBoard);
+	});
+ 	
+	//on disconnect - should eventually declare winner or something idk if we want a reconnect feature
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
 });
 
 http.listen(3000, function(){
--- Battleship_Game_MultiPlayer/public/battleship.js
+++ Battleship_Game_MultiPlayer/public/battleship.js
@@ -2,7 +2,11 @@
 var rows = 10;
 var cols = 10;
 var squareSize = 35;
-
+//set to true once player has placed ships
+var enemyReady = false;
+var playerReady = false;
+//whether or not it is this players turn
+var myTurn = false;
 // get the container element
 var enemyBoardContainer = document.getElementById("enemyBoard");
 var playerBoardContainer = document.getElementById("playerBoard");
@@ -59,6 +63,7 @@ for (i = 0; i < cols; i++) {
       Patrol Boat - 2 hits
 */
 var hitCount = 0;
+var playerHealth = 17;
 
 //ship array
 var ships = [5, 4, 3, 3, 2];
@@ -71,9 +76,9 @@ var enemyBoard = [
 				[0,0,0,1,1,1,1,0,0,0],
 				[0,0,0,0,0,0,0,0,0,0],
 				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,1,0,0,0],
-				[0,0,0,0,0,0,1,0,0,0],
-				[1,0,0,0,0,0,1,1,1,1],
+				[0,0,0,0,0,1,0,0,0,0],
+				[0,0,0,0,0,1,0,0,0,0],
+				[1,0,0,0,0,1,0,1,1,1],
 				[1,0,0,0,0,0,0,0,0,0],
 				[1,0,0,1,0,0,0,0,0,0],
 				[1,0,0,1,0,0,0,0,0,0],
@@ -132,11 +137,17 @@ function placeShip(e) {
 				var len = ships[shipNumber]; 
 				place(len, r, c, horizontal);
 				shipNumber++;
+				//once ship placement is finished, send board configuration to opponent
 				if (shipNumber == 5) {
-					var p = document.getElementById("placeShips");
-					p.style.display = "none";
-					var en = document.getElementById("enemyBoard");
-					en.style.display = "block";
+					socket.emit('transferBoard', playerBoard);
+					playerReady = true;
+					if(playerReady && enemyReady) {
+						//remove the place ships ui and replace it with the opponents board
+						var p = document.getElementById("placeShips");
+						p.style.display = "none";
+						var en = document.getElementById("enemyBoard");
+						en.style.display = "block";
+					}
 				}
 			}
 			//console.log("placeShip4");
@@ -198,36 +209,87 @@ enemyBoardContainer.addEventListener("click", fireTorpedo, false);
 
 // initial code via http://www.kirupa.com/html5/handling_events_for_many_elements.htm:
 function fireTorpedo(e) {
-    // if item clicked (e.target) is not the parent element on which the event listener was set (e.currentTarget)
-	if (e.target !== e.currentTarget) {
-        // extract row and column # from the HTML element's id
-		var row = e.target.id.substring(1,2);
-		var col = e.target.id.substring(2,3);
-        //alert("Clicked on row " + row + ", col " + col);
+	//this function only means something if it is the current players turn
+	if(myTurn) {
+		// if item clicked (e.target) is not the parent element on which the event listener was set (e.currentTarget)
+		if (e.target !== e.currentTarget) {
+			// extract row and column # from the HTML element's id
+			var row = e.target.id.substring(1,2);
+			var col = e.target.id.substring(2,3);
+			//alert("Clicked on row " + row + ", col " + col);
+			
+			//send message to the server that the player selected a target
+			var eid = "p" + row + col;
+			socket.emit('transferTorpedo', eid);
+			
+			// if player clicks a square with no ship, change the color and change square's value
+			if (enemyBoard[row][col] == 0) {
+				e.target.style.background = '#bbb';
+				// set this square's value to 3 to indicate that they fired and missed
+				enemyBoard[row][col] = 3;
+				myTurn = false;
+			// if player clicks a square with a ship, change the color and change square's value
+			} else if (enemyBoard[row][col] == 1) {
+				e.target.style.background = 'red';
+				// set this square's value to 2 to indicate the ship has been hit
+				enemyBoard[row][col] = 2;
+				myTurn = false;
+				// increment hitCount each time a ship is hit
+				hitCount++;
+				// this definitely shouldn't be hard-coded, but here it is anyway. lazy, simple solution:
+				if (hitCount == 17) {
+					alert("All enemy ships have been defeated! You win!");
+				}
 				
-		// if player clicks a square with no ship, change the color and change square's value
-		if (enemyBoard[row][col] == 0) {
-			e.target.style.background = '#bbb';
-			// set this square's value to 3 to indicate that they fired and missed
-			enemyBoard[row][col] = 3;
-			
-		// if player clicks a square with a ship, change the color and change square's value
-		} else if (enemyBoard[row][col] == 1) {
-			e.target.style.background = 'red';
-			// set this square's value to 2 to indicate the ship has been hit
-			enemyBoard[row][col] = 2;
-			
-			// increment hitCount each time a ship is hit
-			hitCount++;
-			// this definitely shouldn't be hard-coded, but here it is anyway. lazy, simple solution:
-			if (hitCount == 17) {
-				alert("All enemy battleships have been defeated! You win!");
-			}
-			
-		// if player clicks a square that's been previously hit, let them know
-		} else if (enemyBoard[row][col] > 1) {
-			alert("Stop wasting your torpedos! You already fired at this location.");
-		}		
-    }
+			// if player clicks a square that's been previously hit, let them know
+			} else if (enemyBoard[row][col] > 1) {
+				alert("Stop wasting your torpedos! You already fired at this location.");
+			}		
+		}
+	} else {
+		alert("Wait your turn!");
+	}
     e.stopPropagation();
 }
+
+//runs when a the server sends a message that a torpedo has been fired at the player
+socket.on('receiveTorpedo', function(eid) {
+	
+	var row = eid.substring(1,2);
+	var col = eid.substring(2,3);
+	myTurn = true;
+	
+	// if enemy clicks a square with no ship, change the color and change square's value
+	if (playerBoard[row][col] == 0) {
+		document.getElementById(eid).style.background = '#bbb';
+		// set this square's value to 3 to indicate that they fired and missed
+		playerBoard[row][col] = 3;
+		
+	// if enemy clicks a square with a ship, change the color and change square's value
+	} else if (playerBoard[row][col] == 1) {
+		document.getElementById(eid).style.background = 'red';
+		// set this square's value to 2 to indicate the ship has been hit
+		playerBoard[row][col] = 2;
+		
+		// increment hitCount each time a ship is hit
+		playerHealth--;
+		// this definitely shouldn't be hard-coded, but here it is anyway. lazy, simple solution:
+		if (playerHealth == 0) {
+			alert("All your ships have been defeated! You lose!");
+		}
+	}
+});
+
+socket.on('receiveBoard', function(newBoard) {
+	enemyBoard = newBoard;
+	enemyReady = true;
+	if(playerReady && enemyReady) {
+		//remove the place ships ui and replace it with the opponents board
+		var p = document.getElementById("placeShips");
+		p.style.display = "none";
+		var en = document.getElementById("enemyBoard");
+		en.style.display = "block";
+		//the player that finishes setting up their board first gets to go first
+		myTurn = true;
+	}
+});
